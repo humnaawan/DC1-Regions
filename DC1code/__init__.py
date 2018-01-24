b5a15c6e9b5fa5369d066aacc7b1dddb3bd8c622
Humna Awan <humna.awan@rutgers.edu>
2018-01-24 14:46:04 -0500

    Added init, setup to import code as a whole.

---
+++ DC1code/__init__.py
@@ -0,0 +1,10 @@
+"""
+Utils for DC1 region selection.
+"""
+
+from .GIFproduction import *
+from .findDC1Chips import *
+from .findDC1Regions import *
+from .intermediates import *
+from .plotFunctions import *
+from .readPython2Pickle import *
---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+import distutils
+from distutils.core import setup
+
+description = "Code for DC1 region/chips selection."
+
+setup(name= "DC1code",
+      version= "0.1",
+      description= description,
+      url= "https://github.com/humnaawan/DC1-Regions",
+      author= "Humna Awan for DESC",
+      author_email= "humna.awan@rutgers.edu",
+      packages= ['DC1code'])
